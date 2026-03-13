075479e762b9cfbeeac86478caf3a10f2b5c9aab
Farag, Mohamed Abdelmonem Ahmed Mahmoud <mfarag8@gatech.edu>
2026-03-13 15:08:42 -0500

    Initial Commit

---
+++ IntentBlock/IntentBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentBlockApp.swift
+//  IntentBlock
+//
+//  Created by Farag on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct IntentBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
